119d138d4e73e3020845edfc10a6a699bb64a1e8
Lvov Aleksandr <prince71188@gmail.com>
2021-11-27 04:40:37 +0300

    Refactoring to other API file

--- components/LoginPage/LoginPage.ts
+++ components/LoginPage/LoginPage.ts
@@ -1,52 +1,7 @@
 import { VDom } from "../../my_core/VDom"
-import Router, { RouterLink } from "../../my_core/router"
-import { state } from "../../my_core/core"
+import { RouterLink } from "../../my_core/router"
+import { login } from "./LoginPageApi";
 
-const host = 'https://ya-praktikum.tech/api/v2'
-
-async function login (e) {
-    e.preventDefault()
-    const loginInput = (document.getElementsByName('login')[0]) as HTMLInputElement
-    const login = loginInput.value
-    const passwordInput = (document.getElementsByName('password')[0]) as HTMLInputElement
-    const password = passwordInput.value
-
-
-
-    const loginResult = (await fetch(`${host}/auth/signin`, {
-        method: 'POST',
-        credentials: 'include',
-        mode: 'cors',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({ login, password }),
-    }))
-
-    if (loginResult.status !== 200) {
-        const error = await loginResult.json()
-        alert(error.reason)
-        return
-    }
-
-    const chatsResult = await fetch(`${host}/chats`, {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-    })
-
-    state.chats = await chatsResult.json()
-
-    const userResult = await fetch(`${host}/auth/user`, {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-    })
-
-    state.user = await userResult.json()
-
-    Router.get().to('/messenger')
-}
 
 export default function LoginPage () {
     return VDom.createElement('div', { className: 'site-wrapper hi-contrast-bg'},
--- components/RegisterPage/RegisterPage.ts
+++ components/RegisterPage/RegisterPage.ts
@@ -1,51 +1,7 @@
 import { VDom } from "../../my_core/VDom";
-import Router, {RouterLink} from "../../my_core/router";
+import {RouterLink} from "../../my_core/router";
+import {register} from "./RegisterPageApi";
 
-const host = 'https://ya-praktikum.tech/api/v2'
-
-async function register (e) {
-    e.preventDefault()
-    const first_nameInput = (document.getElementsByName('first_name')[0]) as HTMLInputElement
-    const first_name = first_nameInput.value
-    const second_nameInput = (document.getElementsByName('second_name')[0]) as HTMLInputElement
-    const second_name = second_nameInput.value
-    const loginInput = (document.getElementsByName('login')[0]) as HTMLInputElement
-    const login = loginInput.value
-    const emailInput = (document.getElementsByName('email')[0]) as HTMLInputElement
-    const email = emailInput.value
-    const phoneInput = (document.getElementsByName('phone')[0]) as HTMLInputElement
-    const phone = phoneInput.value
-    const password1Input = (document.getElementsByName('password1')[0]) as HTMLInputElement
-    const password1 = password1Input.value
-    const password2Input = (document.getElementsByName('password2')[0])as HTMLInputElement
-    const password2 = password2Input.value
-
-    const registerResult = (await fetch(`${host}/auth/signup`, {
-        method: 'POST',
-        credentials: 'include',
-        mode: 'cors',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            first_name,
-            second_name,
-            login,
-            email,
-            phone,
-            password: password1
-        }),
-    }))
-
-    if (registerResult.status !== 200) {
-        const error = await registerResult.json()
-        alert(error.reason)
-        return
-    }
-
-    alert('Регистрация успешна!')
-    Router.get().to('/')
-}
 
 export default function RegisterPage () {
     return VDom.createElement('div', { className: 'site-wrapper hi-contrast-bg'},
